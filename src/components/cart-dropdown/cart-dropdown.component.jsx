70fb9dc1f3e68467bdbd9878034671fcca101665
saurav3232 <sauravkumar48736@gmail.com>
2022-12-16 23:43:20 +0530

    Finished styled Components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,22 +1,26 @@
-import './cart-dropdown.styles.scss';
-import Button from '../button/button.component';
-import { useContext } from 'react';
-import { CartContext } from '../../contexts/cart.context';
-import { useNavigate } from 'react-router-dom';
-import CartItem from '../cart-item/cart-item.component';
+import "./cart-dropdown.styles.scss";
+import Button from "../button/button.component";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+import { useNavigate } from "react-router-dom";
+import CartItem from "../cart-item/cart-item.component";
 const CartDropdown = () => {
-    const {cartItems}=useContext(CartContext);
-    const navigate=useNavigate();
-    const goToCheckouthandler=()=>{
-        navigate('/checkout');
-    }
-    return (
-        <div className='cart-dropdown-container'>
-            <div className="cart-items">
-                {cartItems.map(item=><CartItem key={item.id} cartItem={item}/>)}
-            </div>
-                <Button onClick={goToCheckouthandler}>Go to CheckOut</Button>
-        </div>
-    )
+  const { cartItems, setIsCartOpen } = useContext(CartContext);
+  const navigate = useNavigate();
+  const goToCheckouthandler = () => {
+    navigate("/checkout");
+  };
+  return (
+    <div className="cart-dropdown-container" onMouseLeave={()=>setIsCartOpen(false)}>
+      <div className="cart-items">
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
+      </div>
+      <Button onClick={goToCheckouthandler}>Go to CheckOut</Button>
+    </div>
+  );
 };
 export default CartDropdown;
