eec4f2dd7c49f00863ab99a3b8b9ef6297c4cdbd
saurav3232 <sauravkumar48736@gmail.com>
2022-12-22 19:56:11 +0530

    Added redux persist library

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,25 @@
-import {compose,createStore,applyMiddleware} from 'redux';
-import logger from 'redux-logger';
+
+import { compose, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+// import logger from 'redux-logger';
+
 import { rootReducer } from './root-reducer';
-const middlewares=[logger];
-const composedEnhancers=compose(applyMiddleware(...middlewares));
-export const store=createStore(rootReducer,undefined,composedEnhancers);
+
+import logger from 'redux-logger';
+const persistConfig={
+    key:'root',
+    storage:storage,
+    blacklist:['user']//Since user value is coming from auth state listener
+}
+const persistedReducer=persistReducer(persistConfig,rootReducer);
+const middlewares=[process.env.NODE_ENV!=="production" &&  logger].filter(Boolean);
+
+const composeEnhancer=(process.env.NODE_ENV!=="production" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const composedEnhancers=composeEnhancer(applyMiddleware(...middlewares));
+
+
+
+export const store=createStore(persistedReducer,undefined,composedEnhancers);
+export const persistor=persistStore(store);
