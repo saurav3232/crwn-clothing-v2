7eaae91c993b3405aa31f9af644c07f578140f4d
saurav3232 <sauravkumar48736@gmail.com>
2022-12-22 20:58:56 +0530

    Added loading spinner and thunk

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,23 +1,19 @@
 import "./shop.styles.scss";
-import { useEffect } from "react";
-import { Routes,Route } from "react-router-dom";
-import CategoriesPreview from "../categories-preview/categories-preview.component";
-import Category from "../category/category.component";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategories } from "../../store/categories/category.action";
-import { useDispatch } from "react-redux";
+import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
+import { fetchCategoriesStartAsync } from '../../store/categories/category.action';
+
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray= await getCategoriesAndDocuments('categories');
-      // console.log(categoriesArray); 
-      dispatch(setCategories(categoriesArray));
-    };
-
-    getCategoriesMap();
+    dispatch(fetchCategoriesStartAsync());
   }, []);
+
     return (
         <Routes>
           <Route index element={<CategoriesPreview/>}/>
--- src/store/store.js
+++ src/store/store.js
@@ -1,25 +1,36 @@
-
 import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import { rootReducer } from './root-reducer';
 
-import logger from 'redux-logger';
-const persistConfig={
-    key:'root',
-    storage:storage,
-    blacklist:['user']//Since user value is coming from auth state listener
-}
-const persistedReducer=persistReducer(persistConfig,rootReducer);
-const middlewares=[process.env.NODE_ENV!=="production" &&  logger].filter(Boolean);
+const middleWares = [
+  process.env.NODE_ENV === 'development' && logger,
+  thunk,
+].filter(Boolean);
 
-const composeEnhancer=(process.env.NODE_ENV!=="production" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
-const composedEnhancers=composeEnhancer(applyMiddleware(...middlewares));
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
-export const store=createStore(persistedReducer,undefined,composedEnhancers);
-export const persistor=persistStore(store);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
